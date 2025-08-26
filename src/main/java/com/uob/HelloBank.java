a8e62003698fab3074e0f2c24a21ed635210e15a
Lam Hao Fah <haofah14@gmail.com>
2025-08-26 18:19:07 +0800

    update

--- src/main/java/com/uob/HelloBank.java
+++ src/main/java/com/uob/HelloBank.java
@@ -1,23 +1,18 @@
 package com.uob;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class HelloBank {
 
-    public static void main(String[] args) throws NoSuchAlgorithmException {
-        // VULNERABILITY: Hardcoded password used in authentication
-        String password = "SuperSecret123"; // SonarCloud will flag this
+    public void login(String userInput) throws SQLException {
+        Connection conn = DriverManager.getConnection("jdbc:h2:mem:testdb");
+        Statement stmt = conn.createStatement();
 
-        // Using password in an insecure operation triggers Vulnerability
-        // Here: insecure MD5 hashing combined with hardcoded password
-        MessageDigest md = MessageDigest.getInstance("MD5");
-        byte[] hash = md.digest(password.getBytes());
-
-        // VULNERABILITY: Hardcoded API token (another common trigger)
-        String apiToken = "abc123TOKEN"; // SonarCloud flags hardcoded secret
-        System.out.println("Login with password hash: " + hash + " and token: " + apiToken);
-
-        System.out.println("Welcome to UOB!");
+        // BUG + VULNERABILITY: SQL Injection
+        String query = "SELECT * FROM users WHERE username = '" + userInput + "'";
+        stmt.executeQuery(query);
     }
 }
