44f084e5aa6b35c57cc4a71592168dd0e80af58a
Lam Hao Fah <haofah14@gmail.com>
2025-08-26 18:45:52 +0800

    update

--- src/main/java/com/uob/LoanProcessor.java
+++ src/main/java/com/uob/LoanProcessor.java
@@ -1,10 +1,5 @@
 package com.uob;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
-
 public class LoanProcessor {
 
     public void processLoan() {
@@ -12,19 +7,4 @@ public class LoanProcessor {
         String customer = null;
         System.out.println(customer.length());
     }
-
-    public void processLoan2(String userInput) throws Exception {
-        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/bank", "root", "password");
-        Statement stmt = conn.createStatement();
-
-        // Vulnerability: SQL Injection
-        String query = "SELECT * FROM loans WHERE user='" + userInput + "'";
-        ResultSet rs = stmt.executeQuery(query);
-
-        while (rs.next()) {
-            System.out.println("Loan: " + rs.getString("loan_id"));
-        }
-    }
-
-
 }
